182e3d283b6939a102802aa26ee3f5f5e099a044
mariana <mariana.umanzor.ugb@gmail.com>
2021-07-03 11:55:37 -0600

    Sabado

---
+++ template/repetir.php
@@ -0,0 +1,9 @@
+<?php
+
+echo $_SESSION['sucursal1'].'<hr>';
+echo $_SESSION['sucursal2'].'<hr>';
+echo $_SESSION['sucursal3'].'<hr>';
+echo $_SESSION['sucursal4'].'<hr>';
+echo $_SESSION['sucursal5'].'<hr>';
+
+?>
