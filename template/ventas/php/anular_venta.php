d8084855345b2d98d1e54cefa1145789e947175f
mariana <mariana.umanzor.ugb@gmail.com>
2021-08-26 17:56:31 -0600

    tarde jueves

--- template/ventas/php/anular_venta.php
+++ template/ventas/php/anular_venta.php
@@ -4,11 +4,11 @@ session_start();
 require_once "conexion.php";
 $conexion = conexion();
 
-$codigo = $_POST['codigo'];
-$comentario_recibido = $_POST['comentario'];
-@$tcodigo = $codigo;
-@$tsucursal_codigo = 0;
-@$sucursal = "";
+$codigo = $_GET['codigo'];
+$comentario_recibido = $_GET['comentario'];
+@$vent_codigo = $codigo;
+@$vent_sucursal_codigo = 0;
+@$sucu_nombre = "";
 @$ttipo_codigo = 0;
 @$ttipo ="";
 @$tconcepto = "";
@@ -30,21 +30,21 @@ $tran = "SELECT vv.vent_codigo , vent_codigo_temporal, vent_sucursal_codigo, ss.
 
 $flujo = 0;
 
-$sql_detalle = "select * from ventd_detalle vd where vd.ventd_vent_codigo  = " . $tcodigo . ";";
+$sql_detalle = "select * from ventd_detalle vd where vd.ventd_vent_codigo  = " . $codigo . ";";
 
 $r_maestro = mysqli_query($conexion, $tran);
 $r_detalle = mysqli_query($conexion, $sql_detalle);
 
 while ($item =  mysqli_fetch_assoc($r_maestro)) {
-    @$tcodigo = $item['vent_codigo'];
-    @$tsucursal_codigo = $item['vent_sucursal_codigo'];
-    @$sucursal = $item['sucu_nombre'];
+    @$vent_codigo = $item['vent_codigo'];
+    @$vent_sucursal_codigo = $item['vent_sucursal_codigo'];
+    @$sucu_nombre = $item['sucu_nombre'];
 
     @$tconcepto = trim($item['tran_nombre_concepto']);
     @$tcomentario = trim($item['tran_comentario']);
     @$tusuario = $item['usuario'];
-    @$tfecha = $item['tran_fecha'];
-    @$testado = $item['tran_estado'];
+    @$tfecha = $item['vent_fecha'];
+    @$testado = $item['vent_estado'];
     @$tanulado = trim($item['anulo']);
 }
     $ttipo = "ENTRADA";
@@ -60,34 +60,36 @@ $sql="UPDATE
             vent_fecha_anula=CURRENT_TIMESTAMP,
             vent_comentario = '$comentario_recibido',
             vent_usuario_anula= ".$_SESSION['usua_codigo']." 
-        WHERE vent_codigo='$tcodigo'";
+        WHERE vent_codigo='$vent_codigo'";
 
-$flujo=mysqli_query($conexion,$sql);
+echo $sql.'<br><br><br>';
+
+//$flujo=mysqli_query($conexion,$sql);
 ///     actualiza encabezado
-if($testado == "PROCESADO")
-{
-    if($flujo > 0)
+// if($testado != "PROCESADO")
+// {
+    if($flujo >= 0)
     {
         while ($deta = mysqli_fetch_assoc($r_detalle)){
             $sql_kardex = "INSERT INTO kardex
             (kard_tipo, kard_concepto, kard_fecha, kard_sucursal_codigo,
              kard_producto_codigo, kard_producto_nombre, kard_unidad_codigo,
               kard_unidad, kard_cantidad_unidades, kard_cantidad) 
-            VALUES('".strtoupper(trim($ttipo))."', '"."POR ANULACION VENTA: # $codigo ', CURRENT_TIMESTAMP, '$tsucursal_codigo', '".$deta['vent_producto_codigo']."', '".trim($deta['vent_producto_nombre'])."', ".$deta['vent_unidad_codigo'].", '".trim($deta['vent_unidad'])."', ".$deta['vent_unidad_cantidad'] .", ".$deta['vent_cantidad'].");";
+            VALUES('".strtoupper(trim($ttipo))."', '"."POR ANULACION VENTA: # $codigo ', CURRENT_TIMESTAMP, '$vent_sucursal_codigo', '".$deta['vent_producto_codigo']."', '".trim($deta['vent_producto_nombre'])."', ".$deta['vent_unidad_codigo'].", '".trim($deta['vent_unidad'])."', ".$deta['vent_unidad_cantidad'] .", ".$deta['vent_cantidad'].");";
     
             $total_uniades = $deta['vent_unidad_cantidad'] * $deta['vent_cantidad'];
             $sql_producto = "UPDATE prod_producto
-                            SET  prod_existencia".$tsucursal_codigo." = prod_existencia".$tsucursal_codigo." + ".$total_uniades."
+                            SET  prod_existencia".$vent_sucursal_codigo." = prod_existencia".$vent_sucursal_codigo." + ".$total_uniades."
                             WHERE prod_codigo = '" .$deta['vent_producto_codigo']."'";
     
-            $flujo=mysqli_query($conexion,$sql_kardex);
-            $flujo=mysqli_query($conexion,$sql_producto);
+            // $flujo=mysqli_query($conexion,$sql_kardex);
+            // $flujo=mysqli_query($conexion,$sql_producto);
     
-            // echo $sql_kardex."<br ><br >";
-            // echo $sql_producto."<b"
+            echo $sql_kardex."<br ><br ><br>";
+            echo $sql_producto."<br><br>";
         }
     }
-}
+// }
 $cumulo = "";
 /// procesar detalle
 
